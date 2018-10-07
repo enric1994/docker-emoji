a059624ea2639198672b1adbaca1542c2d0891af
Enric Moreu <enric.moreu.filella@gmail.com>
2018-10-07 13:13:24 +0100

    tag and name suggestions added

--- get_emoji.sh
+++ get_emoji.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-cat /src/emoji_db.json |jq --raw-output --arg tag "$1" ' .[] | select(select([.aliases[] == $tag] | any)) | .emoji'
+response=$(cat /src/emoji_db.json |jq --raw-output --arg tag "$1" ' .[] | select(select([.aliases[] == $tag] | any)) | .emoji')
 
+if [ -z $response ]
+then
+    echo -e 'Emoji not found🤦‍\nMaybe you where looking for...\n'
+    cat /src/emoji_db.json |jq --raw-output --arg tag "$1" '.[] | select(.tags[] | contains($tag)) | "\(.emoji) \(.aliases[0])"'
+    cat /src/emoji_db.json |jq --raw-output --arg tag "$1" '.[] | select(.aliases[] | contains($tag)) | "\(.emoji) \(.aliases[0])"'
+else
+    echo $response
+fi
